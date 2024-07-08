4028f3d9524428153297a0614056735b2853ee9b
Khairiyan Hidayat Ramadhan <khairiyan.hidayat@admo.astra.co.id>
2024-07-08 14:09:32 +0700

    feat: create method Specification for get equal tipePesawat

--- src/main/java/com/project/schedule/specification/PesawatSpecification.java
+++ src/main/java/com/project/schedule/specification/PesawatSpecification.java
@@ -23,4 +23,8 @@ public class PesawatSpecification {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("maskapai"), maskapai);
     }
 
+    public static Specification<Pesawat> hasTipePesawat(String tipePesawat){
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("tipePesawat"), tipePesawat);
+    }
+
 }
