9024dd5d05849d3b07f0c1072825e982272be9f3
Khairiyan Hidayat Ramadhan <khairiyan.hidayat@admo.astra.co.id>
2024-07-08 14:04:39 +0700

    feat: add validate for patch request

--- src/main/java/com/project/schedule/validation/PayloadValidation.java
+++ src/main/java/com/project/schedule/validation/PayloadValidation.java
@@ -3,8 +3,11 @@ package com.project.schedule.validation;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.schedule.constant.GeneralConstant;
 import com.project.schedule.dto.request.PesawatPostRequest;
+import com.project.schedule.dto.request.PesawatUpdateRequest;
 import com.project.schedule.exception.IllegalPayloadException;
 import com.project.schedule.model.Pesawat;
+import com.project.schedule.repository.PesawatRepository;
+import com.project.schedule.specification.PesawatSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +21,12 @@ public class PayloadValidation {
 
     private final ObjectMapper objectMapper;
 
-        public void validatePostRequest(PesawatPostRequest request, Pesawat pesawat) {
+    private final PesawatRepository pesawatRepository;
+
+        public void validatePostRequest(PesawatPostRequest request, boolean existingPesawat) {
             List<String> errors =  new ArrayList<>();
             Pattern patternNumbers = Pattern.compile("^\\d+$");
-            if(pesawat == null){
+            if(!existingPesawat){
                 if (patternNumbers.matcher(request.getMaskapai()).matches()){
                     errors.add(String.format(GeneralConstant.ErrorMessageApi.ILLEGAL_INPUT_PAYLOAD, "maskapai"));
                 }
@@ -29,15 +34,43 @@ public class PayloadValidation {
                     errors.add(String.format(GeneralConstant.ErrorMessageApi.ILLEGAL_INPUT_PAYLOAD, "tipePesawat"));
                 }
             } else {
-                if(pesawat.getTipe_pesawat().equals(request.getTipePesawat()) && pesawat.getMaskapai().equals(request.getMaskapai())){
-                    errors.add(String.format(GeneralConstant.ErrorMessageApi.ILLEGAL_INPUT_SAME_DATA, "maskapai", "tipePesawat"));
-                }
+                errors.add(String.format(GeneralConstant.ErrorMessageApi.ILLEGAL_INPUT_SAME_DATA, "maskapai", "tipePesawat"));
             }
             if (errors.size() > 1){
                 throw new IllegalPayloadException(errors.getFirst() + "%" + errors.getLast());
             }
             if(!errors.isEmpty()){
                 throw new IllegalPayloadException(errors.getFirst());
+            }
+        }
+
+        public void validatePatchRequest(Pesawat pesawat, PesawatUpdateRequest request){
+            boolean checkBoolean = false;
+            if(request.getTipePesawat() != null && request.getMaskapai() != null){
+                checkBoolean = pesawatRepository
+                        .exists(
+                                PesawatSpecification
+                                        .hasTipePesawat(request.getTipePesawat())
+                                        .and(PesawatSpecification.hasMaskapaiName(request.getMaskapai()))
+                        );
+            } else if(request.getTipePesawat() == null && request.getMaskapai() != null){
+                checkBoolean = pesawatRepository
+                        .exists(
+                                PesawatSpecification
+                                        .hasTipePesawat(pesawat.getTipePesawat())
+                                        .and(PesawatSpecification.hasMaskapaiName(request.getMaskapai()))
+                        );
+            } else if(request.getTipePesawat() != null){
+                checkBoolean = pesawatRepository
+                        .exists(
+                                PesawatSpecification
+                                        .hasTipePesawat(request.getTipePesawat())
+                                        .and(PesawatSpecification.hasMaskapaiName(pesawat.getMaskapai()))
+                        );
+            }
+
+            if(checkBoolean){
+                throw new IllegalPayloadException(String.format(GeneralConstant.ErrorMessageApi.ILLEGAL_INPUT_FOR_UPDATE, "maskapai", "tipePesawat"));
             }
         }
 
