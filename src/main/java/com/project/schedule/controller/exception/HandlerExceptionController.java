496a4ad74489f6a777f4561c04ffaa5a182e1ddd
Khairiyan Hidayat Ramadhan <khairiyan.hidayat@admo.astra.co.id>
2024-07-08 14:04:06 +0700

    feat: add NullPointerException custom message

--- src/main/java/com/project/schedule/controller/exception/HandlerExceptionController.java
+++ src/main/java/com/project/schedule/controller/exception/HandlerExceptionController.java
@@ -1,6 +1,7 @@
 package com.project.schedule.controller.exception;
 
 import com.project.schedule.dto.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,10 +11,12 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestControllerAdvice
+@Slf4j
 public class HandlerExceptionController {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse<String[]>> handleIllegalArgumentException(IllegalArgumentException exception){
+        log.info("Error Exception : {}", exception.getLocalizedMessage());
         String[] messages = null;
         if(exception.getMessage().contains("%")){
            messages = exception.getMessage().split("%");
@@ -23,9 +26,17 @@ public class HandlerExceptionController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.<String[]>builder().error(messages).build());
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse<List<String>>> handleDataNotFoundException(RuntimeException exception){
-        List<String> messages = Arrays.asList(exception.getMessage().split("@"));
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.<List<String>>builder().error(messages).build());
+//    @ExceptionHandler(RuntimeException.class)
+//    public ResponseEntity<ErrorResponse<List<String>>> handleDataNotFoundException(RuntimeException exception){
+//        log.info("Error Exception : {}", exception.getLocalizedMessage());
+//        List<String> messages = Arrays.asList(exception.getMessage().split("@"));
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.<List<String>>builder().error(messages).build());
+//   }
+
+   @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ErrorResponse<List<String>>> handleNullPointerException(NullPointerException exception){
+        log.info("Error Exception : {}", exception.getLocalizedMessage());
+        List<String> messages = Arrays.asList(exception.getMessage().split(","));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.<List<String>>builder().error(messages).build());
    }
 }
