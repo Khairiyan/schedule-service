aded301fb9ebf135aec816dc67ad88b778cf50b4
Khairiyan Hidayat Ramadhan <khairiyan.hidayat@admo.astra.co.id>
2024-07-08 14:05:50 +0700

    feat: add nonnull validation for create Plane API

--- src/main/java/com/project/schedule/dto/request/PesawatPostRequest.java
+++ src/main/java/com/project/schedule/dto/request/PesawatPostRequest.java
@@ -1,11 +1,14 @@
 package com.project.schedule.dto.request;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class PesawatPostRequest {
 
+    @NonNull
     private String maskapai;
 
+    @NonNull
     private String tipePesawat;
 }
