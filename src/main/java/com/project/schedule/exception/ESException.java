9fd0723bac26a4ee0b76b5a03846744cfa4b3aeb
Khairiyan Hidayat Ramadhan <khairiyan.hidayat@admo.astra.co.id>
2024-07-10 17:05:27 +0700

    feat: add Exception to elasticsearch

---
+++ src/main/java/com/project/schedule/exception/ESException.java
@@ -0,0 +1,12 @@
+//package com.project.schedule.exception;
+//
+//import co.elastic.clients.elasticsearch._types.ElasticsearchException;
+//import co.elastic.clients.elasticsearch._types.ErrorResponse;
+//
+//public class ESException extends ElasticsearchException {
+//
+//
+//    public ESException(String endpointId, ErrorResponse response) {
+//        super(endpointId, response);
+//    }
+//}
