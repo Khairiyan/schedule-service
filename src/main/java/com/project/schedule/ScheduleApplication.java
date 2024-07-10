1d7313d54990970bf55605bca768a3cd52d98d47
Khairiyan Hidayat Ramadhan <khairiyan.hidayat@admo.astra.co.id>
2024-07-10 16:58:57 +0700

    feat: add annotation for scanning the config properties

--- src/main/java/com/project/schedule/ScheduleApplication.java
+++ src/main/java/com/project/schedule/ScheduleApplication.java
@@ -1,9 +1,15 @@
 package com.project.schedule;
 
+import com.project.schedule.config.ESConfiguration;
+import com.project.schedule.config.properties.ESConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ScheduleApplication {
 
 	public static void main(String[] args) {
